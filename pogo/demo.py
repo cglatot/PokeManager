90d0e16af99f5b086fd074a88912e4b475237754
cglatot <ciaran@thegallagherclan.co.uk>
2016-07-27 12:59:41 +0100

    Show number of times Pokemon can be evolved
    Credit to @kvanderkamp  for the idea and code. Also has some artifacts
    from trying to include the Move Names in View Pokemon.

--- pogo/demo.py
+++ pogo/demo.py
@@ -195,9 +195,14 @@ def viewCounts(session):
 	sortBy = int(raw_input('How to sort the list? (1 = Alphabetically, 2 = Total Numbers): '))
 	countList.sort(key = operator.itemgetter(sortBy -1))
 	
+	# Total number of Pokemon that can be evolved
+	# Number of evolutions per Pokemon
+	countEvolutions = 0
+	evolutions = 0
+	
 	# Print the list of pokemon in a nicer format
-	print '\n NAME            | COUNT | CANDIES '
-	print '---------------- | ----- | ------- '
+	print '\n NAME            | COUNT | CANDIES | EVOLVE '
+	print '---------------- | ----- | ------- | ------ '
 	for monster in countList:
 		pokedexNum = getattr(pokedex, monster[0])
 		try:
@@ -210,7 +215,12 @@ def viewCounts(session):
 					candies = session.checkInventory().candies[pokedexNum - 2]
 				except:
 					candies = 0
-		print ' %-15s | %-5d | %d ' % (monster[0], monster[1], candies)
+
+		if(pokedex.evolves[pokedexNum]):
+			evolutions = min(monster[1],int((candies-1)/pokedex.evolves[pokedexNum]))
+			if evolutions > 0:
+				countEvolutions += evolutions
+		print ' %-15s | %-5d | %-7d | %d ' % (monster[0], monster[1], candies, evolutions)
 	
 	mainMenu(session)
 	
@@ -221,7 +231,7 @@ def viewPokemon(session):
 	# Get the party and put it into a nicer list
 	for pokemon in party:
 		IvPercent = ((pokemon.individual_attack + pokemon.individual_defense + pokemon.individual_stamina)*100)/45
-		L = [pokedex[pokemon.pokemon_id],pokemon.cp,pokemon.individual_attack,pokemon.individual_defense,pokemon.individual_stamina,IvPercent,pokemon]
+		L = [pokedex[pokemon.pokemon_id],pokemon.cp,pokemon.individual_attack,pokemon.individual_defense,pokemon.individual_stamina,IvPercent,pokemon]#,pokemon.move_1,pokemon.move_2]
 		myParty.append(L)
 	
 	# Sort party by name and then IV percentage	
@@ -236,11 +246,11 @@ def viewPokemon(session):
 			if myParty[i][0] != myParty[i-1][0]:
 				print '---------------- | ----- | --- | --- | --- | ----'
 		if monster[5] > 74:
-			logging.info('\033[1;32;40m %-15s | %-5s | %-3s | %-3s | %-3s | %-3s \033[0m',monster[0],monster[1],monster[2],monster[3],monster[4],monster[5])
+			logging.info('\033[1;32;40m %-15s | %-5s | %-3s | %-3s | %-3s | %-3s | %-15s | %s \033[0m',monster[0],monster[1],monster[2],monster[3],monster[4],monster[5])#,monster[7],monster[8])
 		elif monster[5] > 49:
-			logging.info('\033[1;33;40m %-15s | %-5s | %-3s | %-3s | %-3s | %-3s \033[0m',monster[0],monster[1],monster[2],monster[3],monster[4],monster[5])
+			logging.info('\033[1;33;40m %-15s | %-5s | %-3s | %-3s | %-3s | %-3s | %-15s | %s \033[0m',monster[0],monster[1],monster[2],monster[3],monster[4],monster[5])#,monster[7],monster[8])
 		else:
-			logging.info('\033[1;37;40m %-15s | %-5s | %-3s | %-3s | %-3s | %-3s \033[0m',monster[0],monster[1],monster[2],monster[3],monster[4],monster[5])
+			logging.info('\033[1;37;40m %-15s | %-5s | %-3s | %-3s | %-3s | %-3s | %-15s | %s \033[0m',monster[0],monster[1],monster[2],monster[3],monster[4],monster[5])#,monster[7],monster[8])
 		i = i+1
 
 	mainMenu(session)
